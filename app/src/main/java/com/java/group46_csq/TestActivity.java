ee090ab06f7d99004eaaf6872a483e090ad69e56
latsumi <375136282@qq.com>
2017-09-12 23:41:49 +0800

    左侧边栏的点击响应小bug修正

--- app/src/main/java/com/java/group46_csq/TestActivity.java
+++ app/src/main/java/com/java/group46_csq/TestActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-public class TestActivity extends AppCompatActivity{
+public class TestActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
           super.onCreate(savedInstanceState);
